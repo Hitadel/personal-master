c6d94c9f8419b1ee741f30ae156114d10f0e88b3
Hitadel <jinha6963@gmail.com>
2023-05-19 11:14:21 +0900

    20230519 1114

--- src/controller/plannerController.js
+++ src/controller/plannerController.js
@@ -1,53 +1,78 @@
-// const { Planner } = require("../models");
-import Planner from "../models/ExercisePlan";
+import ExercisePlanner from "../models/ExercisePlan";
+import NutritionPlanner from "../models/NutritionPlan";
 
-// exports.createExercisePlan = async (req, res, next) => {
-//   try {
-//     const { plan } = req.body;
-//     const parsedPlan = JSON.parse(plan);
-//     console.log('플랜확인:',parsedPlan);
-//     console.log('길이 확인:',parsedPlan.length)
-
-//     // const user_id = req.user.id; // 사용자 ID는 인증된 사용자로부터 얻을 수 있어야 함
-
-//     // Plan 배열의 각 요소에 대해 DB에 저장
-//     for (let i = 0; i < parsedPlan.length; i++) {
-//       const { date, exercise, set, count } = parsedPlan[i];
-//       await Planner.create({
-//         user_id: req.user.id,
-//         date,
-//         exercise,
-//         set,
-//         count,
-//       });
-//     }
-
-//     res.status(200).json({ message: "Exercise plan created successfully." });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Internal server error." });
-//   }
-// };
-export const createExercisePlan = async (req, res, next) => {
+const createPlan = async (req, res, next, model) => {
   try {
     const { plan } = req.body;
-    const parsedPlan = JSON.parse(plan); // plan을 파싱하여 JSON 객체로 변환
-    console.log("플랜확인:", parsedPlan);
-    console.log("길이 확인:", parsedPlan.length);
-    // const user_id = req.user.id; // 사용자 ID는 인증된 사용자로부터 얻을 수 있어야 함
+    console.log("플랜확인:", plan);
+    console.log("길이 확인:", plan.length);
+
     // Plan 배열의 각 요소에 대해 DB에 저장
-    for (let i = 0; i < parsedPlan.length; i++) {
-      const { date, exercise, set, count } = parsedPlan[i];
-      await Planner.create({
+    for (let i = 0; i < plan.length; i++) {
+      const { createdAt } = plan[i];
+      let additionalData = {};
+
+      if (model === NutritionPlanner) {
+        const { name, calorie = 0, protein = 0, fat = 0, cho = 0 } = plan[i];
+        additionalData = {
+          name,
+          calorie: calorie !== null ? calorie : 0, // 혹여 null값이 전달 되도 0이 입력되게 끔
+          protein: protein !== null ? protein : 0,
+          fat: fat !== null ? fat : 0,
+          cho: cho !== null ? cho : 0,
+        };
+      } else if (model === ExercisePlanner) {
+        const { type, count, set } = plan[i];
+        additionalData = { type, count, set };
+      }
+
+      await model.create({
         user_id: req.user.id,
-        date,
-        exercise,
-        set,
-        count,
+        createdAt,
+        ...additionalData,
       });
     }
+    res.status(200).json({ message: `plan created successfully.` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
 
-    res.status(200).json({ message: "Exercise plan created successfully." });
+export const createExercisePlan = async (req, res, next) => {
+  await createPlan(req, res, next, ExercisePlanner);
+};
+
+export const createNutritionPlan = async (req, res, next) => {
+  await createPlan(req, res, next, NutritionPlanner);
+};
+
+export const planCheck = async (req, res, next) => {
+  try {
+    const { date, partition } = req.body;
+    let model;
+    let message;
+
+    if (partition === "exercise") {
+      model = ExercisePlanner;
+      message = "exercisePlan";
+    } else if (partition === "nutrition") {
+      model = NutritionPlanner;
+      message = "nutritionPlan";
+    }
+
+    const existingPlan = await model.findOne({
+      user_id: req.user.id,
+      createAt: date,
+    });
+
+    if (existingPlan) {
+      // 이미 해당 날짜에 계획이 있는 경우
+      res.status(200).json({ message: `${message} already exists.`, confirm: false });
+    } else {
+      // 해당 날짜에 계획이 없는 경우
+      res.status(200).json({ message: `${message} does not exist.`, confirm: true });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error." });
--- src/router/PlannerRouter.js
+++ src/router/PlannerRouter.js
@@ -1,10 +1,11 @@
 import passport from "passport";
 const express = require("express");
-const plannerController = require("../controller/PlannerController");
+const plannerController = require("../controller/plannerController");
 
 const PlannerRouter = express.Router();
 
-// /planner/exercise 경로로 POST 요청이 오면 createExercisePlan 함수를 실행.
-PlannerRouter.post("/exercise", passport.authenticate('jwt', { session: false }), plannerController.createExercisePlan);
+PlannerRouter.post("/exercise", passport.authenticate("jwt", { session: false }), plannerController.createExercisePlan);
+PlannerRouter.post("/nutrition", passport.authenticate("jwt", { session: false }), plannerController.createNutritionPlan);
+PlannerRouter.post("/planCheck", passport.authenticate("jwt", { session: false }), plannerController.planCheck);
 
-module.exports = PlannerRouter;
+export default PlannerRouter;
